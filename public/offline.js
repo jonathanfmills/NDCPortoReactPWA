04d734d987a7eae330b46bf1d9b84dd439ddbc31
Jonathan Mills <jonathan.mills@wwt.com>
2019-02-28 16:03:02 +0000

    Network First

--- public/offline.js
+++ public/offline.js
@@ -11,9 +11,12 @@ self.addEventListener('install', function(evt) {
 
 // On fetch, use cache only strategy.
 self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.');
-  evt.respondWith(fromCache(evt.request));
-});
+    console.log('The service worker is serving the asset.');
+    // Try network and if it fails, go for the cached copy.
+    evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
+      return fromCache(evt.request);
+    }));
+  });
 
 // Open a cache and use `addAll()` with an array of assets to add all of them
 // to the cache. Return a promise resolving when all the assets are added.
@@ -38,7 +41,20 @@ function precache() {
     ]);
   });
 }
-
+// Time limited network request. If the network fails or the response is not
+// served before timeout, the promise is rejected.
+function fromNetwork(request, timeout) {
+    return new Promise(function (fulfill, reject) {
+      // Reject in case of timeout.
+      var timeoutId = setTimeout(reject, timeout);
+      // Fulfill in case of success.
+      fetch(request).then(function (response) {
+        clearTimeout(timeoutId);
+        fulfill(response);
+      // Reject also if network fetch rejects.
+      }, reject);
+    });
+  }
 // Open the cache where the assets were stored and search for the requested
 // resource. Notice that in case of no matching, the promise still resolves
 // but it does with `undefined` as value.
